9e4b3f6685f810eaf9aa8f3650dd7dadd6e6b93e
sameer <sameer>
2020-07-08 01:27:42 +0530

    deleted all files

--- src/main/java/com/poc/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.poc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.poc.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
